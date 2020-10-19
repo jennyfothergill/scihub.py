3b6795cbdaa1469391c1c1d4729895ce48a3fa94
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2020-10-19 17:11:49 -0600

    entrypoints working!
    I think the '.' in the cmd name was screwing it up

--- setup.py
+++ setup.py
@@ -10,11 +10,14 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/zaytoun/scihub.py",
-    packages=setuptools.find_packages(),
+    packages=["scihub"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    zip_safe=False,
+    entry_points = {
+        'console_scripts': ['scihub=scihub.scihub:main']
+        }
 )

a5b126e1a0535e27cccfd9f029a7b4fb4088f321
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2020-10-19 16:37:57 -0600

    blacked and isorted

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="scihub.py-jennyfothergill", # Replace with your own username
+    name="scihub.py-jennyfothergill",  # Replace with your own username
     version="0.0.1",
     description="A pip installable fork of https://github.com/zaytoun/scihub.py",
     long_description=long_description,
@@ -16,6 +16,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
-
